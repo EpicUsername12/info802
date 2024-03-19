50e629fbeee55819dd1eece9def661ab689a8266
Rambo6Glaz <39063367+EpicUsername12@users.noreply.github.com>
2024-03-19 13:21:50 +0100

    Hot fix SOAP url

--- client/utils/SOAP_URL.ts
+++ client/utils/SOAP_URL.ts
@@ -1,4 +1,4 @@
-const SOAP_URL = process.env.SOAP_PUBLIC_URL;
+const SOAP_URL = process.env.SOAP_PUBLIC_URL as string;
 
 if (!SOAP_URL) throw new Error("SOAP_PUBLIC_URL is not defined in the environment variables.");
 
