68e9fb625ceea6caf5dd9275921dec22907166ae
Rambo6Glaz <39063367+EpicUsername12@users.noreply.github.com>
2024-03-19 13:20:00 +0100

    Update SOAP_URL.ts to handle missing environment variable

--- client/utils/SOAP_URL.ts
+++ client/utils/SOAP_URL.ts
@@ -1,3 +1,5 @@
-const SOAP_URL = process.env.SOAP_PUBLIC_URL || "http://localhost:22220";
+const SOAP_URL = process.env.SOAP_PUBLIC_URL;
+
+if (!SOAP_URL) throw new Error("SOAP_PUBLIC_URL is not defined in the environment variables.");
 
 export default SOAP_URL;
