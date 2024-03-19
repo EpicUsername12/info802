a6cc60e4caafe7ab64ea23a51a96e1b045724638
Rambo6Glaz <39063367+EpicUsername12@users.noreply.github.com>
2024-03-19 13:29:09 +0100

    Add Content-Security-Policy meta tag and update error message in SOAP_URL.ts

--- client/utils/SOAP_URL.ts
+++ client/utils/SOAP_URL.ts
@@ -1,5 +1,5 @@
 const SOAP_URL = process.env.NEXT_PUBLIC_SOAP_URL as string;
 
-if (!SOAP_URL) throw new Error("SOAP_PUBLIC_URL is not defined in the environment variables.");
+if (!SOAP_URL) throw new Error("NEXT_PUBLIC_SOAP_URL is not defined in the environment variables.");
 
 export default SOAP_URL;
